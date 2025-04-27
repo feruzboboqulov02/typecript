224c722c32979cf4c85b16d02c9185e6efb9ef5e
feruzboboqulov02 <feruzboboqulov002@gmail.com>
2025-04-27 11:55:38 +0500

    ts_comment

---
+++ app.js
@@ -0,0 +1,26 @@
+"use strict";
+//  //@ts-ignore
+//  function getName(firstName:string,lastName:string):string{
+//     return (`Hello ${firstName} ${lastName}`);
+//  }
+//  console.log(getName("John","Doe"));
+//  const checkAdult=(age:number):boolean=>{
+//     return age>=18?true:false;
+//  }
+//  console.log(checkAdult(20));
+// const person = {
+//     name: "feruzjon",
+//     lastname: "bbklv",
+//     age: 23,
+//     skills: {
+//         programming: "JavaScript",
+//         design: "Photoshop",
+//     },
+// }
+// function getName(data: {name:string,lastname:string}):string{
+//     return(`Hello ${data.name} ${data.lastname}`);
+// }
+// console.log(getName(person))
+const person = ['samar', 23, 87];
+console.log(person[0]);
+console.log(person[1]);
---
+++ app.ts
@@ -0,0 +1,92 @@
+//  //@ts-ignore
+ 
+ 
+//  function getName(firstName:string,lastName:string):string{
+//     return (`Hello ${firstName} ${lastName}`);
+//  }
+
+
+//  console.log(getName("John","Doe"));
+ 
+//  const checkAdult=(age:number):boolean=>{
+//     return age>=18?true:false;
+//  }
+
+//  console.log(checkAdult(20));
+ 
+
+// const person = {
+//     name: "feruzjon",
+//     lastname: "bbklv",
+//     age: 23,
+//     skills: {
+//         programming: "JavaScript",
+//         design: "Photoshop",
+//     },
+// }
+
+// function getName(data: {name:string,lastname:string}):string{
+//     return(`Hello ${data.name} ${data.lastname}`);
+// }
+
+// console.log(getName(person))
+
+// const person : [string,number,number] =['samar',23,87]
+// console.log(person[0])
+// console.log(person[1]) 
+
+// const odam : (string|number)[]= ['samar',23,87] 
+
+
+// enum WebSite{
+//     Facebook = "https://www.facebook.com",
+//     Instagram = "https://www.instagram.com",
+//     Twitter = "https://www.twitter.com",
+//     LinkedIn = "https://www.linkedin.com",
+//     YouTube = "https://www.youtube.com",
+//     TikTok = "https://www.tiktok.com",
+//     Snapchat = "https://www.snapchat.com",
+//     Pinterest = "https://www.pinterest.com",
+//     Reddit = "https://www.reddit.com",
+//     Tumblr = "https://www.tumblr.com"
+// }
+
+// let person: {
+//     id: number,
+//     name: string,
+//     username: string,
+//     email: string,
+//     address: {
+//         street: string,
+//         city: string,
+//         suite: string
+//         zipcode: string|number,
+//         geo:[number,number]
+//     },
+//     phone: number,
+//     website: WebSite,
+//     company: {
+//         name: string,
+//         catchPhrase: string,
+//         bs: string
+//     }
+// } ={
+//     "id": 1,
+//     "name": "Leanne Graham",
+//     "username": "Bret",
+//     "email": "Sincere@april.biz",
+//     "address": {
+//       "street": "Kulas Light",
+//       "suite": "Apt. 556",
+//       "city": "Gwenborough",
+//       "zipcode": "92998-3874",
+//       "geo": [-37.3159,81.1496]
+//     },
+//     "phone": 17707368031,
+//     "website": WebSite.Facebook,
+//     "company": {
+//       "name": "Romaguera-Crona",
+//       "catchPhrase": "Multi-layered client-server neural-net",
+//       "bs": "harness real-time e-markets"
+//     }
+//   }
